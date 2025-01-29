5b9bb74922574bdec278dcb148f2a1043e59f105
dgmedvedev <dg-medvedev@mail.ru>
2025-01-29 12:53:12 +0300

    json is loaded

--- app/src/main/java/com/medvedev/jobsearch/app/presentation/MainFragment.kt
+++ app/src/main/java/com/medvedev/jobsearch/app/presentation/MainFragment.kt
@@ -1,13 +1,39 @@
 package com.medvedev.jobsearch.app.presentation
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.medvedev.jobsearch.R
+import com.medvedev.jobsearch.data.network.ApiFactory
 import com.medvedev.jobsearch.databinding.FragmentMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
     private val binding by viewBinding(FragmentMainBinding::bind)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        testNetwork()
+    }
 
+    private fun testNetwork() {
+        val apiService = ApiFactory.apiService
+
+        val jsonDeferred = lifecycleScope.async {
+            withContext(Dispatchers.IO) {
+                apiService.getJson()
+            }
+        }
+        lifecycleScope.launch {
+            val json = jsonDeferred.await()
+            Log.d("TEST_NETWORK", "$json")
+        }
+    }
 }
--- app/src/main/java/com/medvedev/jobsearch/data/network/ApiFactory.kt
+++ app/src/main/java/com/medvedev/jobsearch/data/network/ApiFactory.kt
@@ -3,30 +3,14 @@ package com.medvedev.jobsearch.data.network
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ApiFactory {
+object ApiFactory {
 
-    companion object {
+    private const val BASE_URL = "https://drive.usercontent.google.com/"
 
-        private const val BASE_URL = "https://drive.usercontent.google.com/"
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
 
-        @Volatile
-        private var instanceApiService: ApiService? = null
-
-        fun getInstanceApiService(): ApiService {
-            instanceApiService?.let { return it }
-            synchronized(ApiFactory::class.java) {
-                instanceApiService?.let { return it }
-
-                val retrofit = Retrofit.Builder()
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .baseUrl(BASE_URL)
-                    .build()
-
-                val apiService = retrofit.create(ApiService::class.java)
-                instanceApiService = apiService
-
-                return apiService
-            }
-        }
-    }
+    val apiService: ApiService = retrofit.create(ApiService::class.java)
 }
