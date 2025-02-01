4f2f442f1b44110f4035fdb9e3520d5ef457ff3f
dgmedvedev <dg-medvedev@mail.ru>
2025-02-01 12:42:06 +0300

    feature/repository is completed. JobRepositoryImpl.kt is created.

--- app/src/main/java/com/medvedev/jobsearch/data/repository/JobRepositoryImpl.kt
+++ app/src/main/java/com/medvedev/jobsearch/data/repository/JobRepositoryImpl.kt
@@ -1,21 +1,19 @@
 package com.medvedev.jobsearch.data.repository
 
+import com.medvedev.jobsearch.data.mapper.toOfferDomainList
+import com.medvedev.jobsearch.data.mapper.toVacancyDomainList
 import com.medvedev.jobsearch.data.network.ApiService
 import com.medvedev.jobsearch.domain.model.offer.Offer
 import com.medvedev.jobsearch.domain.model.vacancy.Vacancy
-import com.medvedev.jobsearch.domain.model.vacancy.VacancyFavorite
 import com.medvedev.jobsearch.domain.repository.JobRepository
 
 class JobRepositoryImpl(private val apiService: ApiService) : JobRepository {
-    override suspend fun getOffers(): List<Offer> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getOffers(): List<Offer> =
+        apiService.getOffers().offers?.toOfferDomainList() ?: listOf()
 
-    override suspend fun getVacancies(): List<Vacancy> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getVacancies(): List<Vacancy> =
+        apiService.getVacancies().vacancies?.toVacancyDomainList() ?: listOf()
 
-    override suspend fun getVacanciesFavorite(): List<VacancyFavorite> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getVacanciesFavorite(): List<Vacancy> =
+        getVacancies().filter { vacancy -> vacancy.isFavorite }
 }
